8eaff7e6903466609cc509e6713f40006b6dd8c0
Mehchu <dtmackenzie06@outlook.com>
2024-02-29 20:58:00 +0000

    Fixed the error catching

--- board/notation_handling.py
+++ board/notation_handling.py
@@ -3,13 +3,16 @@ import numpy as np
 
 class InvalidNotation(Exception):
     def __init__(self, notation):
-        self.message = f"{notation} is invalid move notation"
-        super().__init(self.message)
+        self.message = f'"{notation}" is invalid move notation'
+        super().__init__(self.message)
 
 
 def decompose_notation(notation):
-    start_square = decode_square(notation[:2])
-    end_square = decode_square(notation[2:4])
+    try:
+        start_square = decode_square(notation[:2])
+        end_square = decode_square(notation[2:4])
+    except ValueError:
+        raise InvalidNotation(notation)
     try:
         promotion_piece = notation[4]
     except IndexError:
@@ -19,19 +22,11 @@ def decompose_notation(notation):
 
 
 def encode_square(numerical_square):
-    try:
-        return chr(np.uint64(numerical_square % 8 + 97)) + str(
-            numerical_square // 8 + 1
-        )
-    except ValueError:
-        raise InvalidNotation
+    return chr(np.uint64(numerical_square % 8 + 97)) + str(numerical_square // 8 + 1)
 
 
 def decode_square(algebraic_square):
-    try:
-        return (ord(algebraic_square[0]) - 97) + 8 * (int(algebraic_square[1]) - 1)
-    except ValueError:
-        raise InvalidNotation
+    return (ord(algebraic_square[0]) - 97) + 8 * (int(algebraic_square[1]) - 1)
 
 
 def isOnBoard(square):
